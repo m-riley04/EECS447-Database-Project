02e6598215ebb156001bf5523822d05984c9f10b
m-riley04 <rileymeyerkorth@gmail.com>
2025-04-09 13:38:50 -0500

    add mariadb escape characters

--- sql/tables/t_genre.sql
+++ sql/tables/t_genre.sql
@@ -1,4 +1,4 @@
 CREATE TABLE genre(  
     genre_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name NVARCHAR(50) NOT NULL CHECK (LENGTH(name) >= 1)
+    `name` NVARCHAR(50) NOT NULL CHECK (LENGTH(name) >= 1)
 );
