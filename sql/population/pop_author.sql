fed0a8ff4b0fcd75df520c084619bb9535a18fd1
m-riley04 <rileymeyerkorth@gmail.com>
2025-04-09 13:21:40 -0500

    add population folder and skeleton files

---
+++ sql/population/pop_author.sql
@@ -0,0 +1,15 @@
+INSERT INTO author (first_name, last_name)
+VALUES
+    ('George', 'Orwell'),
+    ('Stephen', 'King'),
+    ('Charles', 'Dickens'),
+    ('Edgar Allen', 'Poe'),
+    ('Isaac', 'Asimov'),
+    ('William', 'Shakespeare'),
+    ('Margaret', 'Atwood'),
+    ('George R.R.', 'Martin'),
+    ('J.K.', 'Rowling'),
+    ('Stan', 'Lee'),
+    ('R.L.', 'Stine'),
+    ('James', 'Patterson'),
+    ('John', 'Irving');
---
+++ sql/population/pop_genre.sql
@@ -0,0 +1,10 @@
+INSERT INTO genre (name)
+VALUES
+    ('Sci-Fi'),
+    ('Romance'),
+    ('Fantasy'),
+    ('Thriller'),
+    ('Mystery'),
+    ('Young Adult'),
+    ('Self-Help'),
+    ('Poetry');
