72a844d63b9106043db713dcbf40b4f8dd2c2901
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-10 13:15:22 -0500

    comment about missing tables

--- sql/tables/t_user.sql
+++ sql/tables/t_user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user(  
     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    membership_type_id INT FOREIGN KEY REFERENCES membership_type(membership_type_id),
-    account_status_id INT FOREIGN KEY REFERENCES account_status(account_status_id),
+    membership_type_id INT FOREIGN KEY REFERENCES membership_type(membership_type_id), -- Need to wait until this table is in
+    account_status_id INT FOREIGN KEY REFERENCES account_status(account_status_id), -- Need to wait until this table is in
     first_name NVARCHAR(100) NOT NULL CHECK (LENGTH(first_name) >= 1),
     last_name NVARCHAR(100) NOT NULL CHECK (LENGTH(first_name) >= 1),
     email NVARCHAR(320) NOT NULL CHECK (email LIKE '%_@__%.__'),
