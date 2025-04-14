605de9479b62f136f66dbfd38b8620919a5e3262
Brett Suhr <145497276+BrettSuhr@users.noreply.github.com>
2025-04-13 22:28:53 -0500

    Create t_transactions.sql
    added the transactions table

---
+++ sql/tables/t_transactions.sql
@@ -0,0 +1,11 @@
+CREATE TABLE transaction_table (
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    media_id INT NOT NULL,
+    checkout_date DATE NOT NULL,
+    due_date DATE NOT NULL,
+    return_date DATE,
+
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (media_id) REFERENCES media_item(media_id)
+);
