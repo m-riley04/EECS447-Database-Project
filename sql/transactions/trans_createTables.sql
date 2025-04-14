84c21d414eadda29fd2b2ef2822c6c78a325278e
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-13 20:37:20 -0500

    create some transactions for easily resetting/creating tables

---
+++ sql/transactions/trans_createTables.sql
@@ -0,0 +1,44 @@
+START TRANSACTION;
+
+-- From the t_author.sql file
+CREATE TABLE author(  
+    author_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name NVARCHAR(100) NOT NULL,
+    last_name NVARCHAR(100) NOT NULL
+);
+
+-- From the t_genre.sql file
+CREATE TABLE genre(  
+    genre_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` NVARCHAR(50) NOT NULL CHECK (LENGTH(name) >= 1)
+);
+
+-- From the t_media_type.sql file
+CREATE TABLE media_type (
+    media_type_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    media_type_name VARCHAR(50) NOT NULL CHECK (CHAR_LENGTH(media_type_name) >= 1)
+);
+
+-- From the t_membership_type.sql file
+CREATE TABLE membership_type(
+    membership_type_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    membership_type_name NVARCHAR(50) NOT NULL CHECK (LENGTH(membership_type_name) >= 1)
+);
+
+-- From the t_media_item.sql file
+CREATE TABLE media_item (
+    media_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    author_id INT,
+    genre_id INT,
+    media_type_id INT,
+    title VARCHAR(255) NOT NULL CHECK (CHAR_LENGTH(title) >= 1),
+    publication_year INT NOT NULL CHECK (publication_year BETWEEN 1900 AND 2025),
+    availability TINYINT NOT NULL CHECK (availability IN (0, 1)),
+    isbn VARCHAR(13) NOT NULL CHECK (CHAR_LENGTH(isbn) = 13 AND isbn REGEXP '^(978|979)[0-9]+$'),
+    
+    CONSTRAINT `fk_author_id` FOREIGN KEY (author_id) REFERENCES author(author_id),
+    CONSTRAINT `fk_genre_id` FOREIGN KEY (genre_id) REFERENCES genre(genre_id),
+    CONSTRAINT `fk_media_type_id` FOREIGN KEY (media_type_id) REFERENCES media_type(media_type_id)
+);
+
+COMMIT;
