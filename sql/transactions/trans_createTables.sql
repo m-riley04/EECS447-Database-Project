490d3078f5e1b04eadafdf2fd006879fa12b2044
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-15 11:42:34 -0500

    add tables to createTables transaction

--- sql/transactions/trans_createTables.sql
+++ sql/transactions/trans_createTables.sql
@@ -41,4 +41,54 @@ CREATE TABLE media_item (
     CONSTRAINT `fk_media_type_id` FOREIGN KEY (media_type_id) REFERENCES media_type(media_type_id)
 );
 
+-- From the t_account_status.sql file
+CREATE TABLE account_status (
+    account_status_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    status_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+-- From the t_user.sql file
+CREATE TABLE `user`(  
+    user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    membership_type_id INT NOT NULL, 
+    account_status_id INT NOT NULL, 
+    first_name NVARCHAR(100) NOT NULL CHECK (LENGTH(first_name) >= 1),
+    last_name NVARCHAR(100) NOT NULL CHECK (LENGTH(first_name) >= 1),
+    email NVARCHAR(320) NOT NULL CHECK (email LIKE '%_@__%.__'),
+    phone NVARCHAR(10) NOT NULL CHECK (LENGTH(phone) = 10 and phone REGEXP '^[0-9]+$'),
+    is_staff BIT NOT NULL,
+
+    CONSTRAINT `fk_membership_type_id` FOREIGN KEY (membership_type_id) REFERENCES membership_type(membership_type_id),
+    CONSTRAINT `fk_account_status_id` FOREIGN KEY (account_status_id) REFERENCES account_status(account_status_id)
+);
+
+CREATE TABLE fee_status (
+    fee_status_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name NVARCHAR(50) NOT NULL,
+    description NVARCHAR(300)
+);
+
+CREATE TABLE fee (
+    fee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    date_issued DATETIME NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    fee_status_id INT NOT NULL,
+    CONSTRAINT `fk_user_id` FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT `fk_fee_status_id` FOREIGN KEY (fee_status_id) REFERENCES fee_status(fee_status_id)
+);
+
+
+CREATE TABLE `transaction` (
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    media_id INT NOT NULL,
+    checkout_date DATE NOT NULL,
+    due_date DATE NOT NULL,
+    return_date DATE,
+
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (media_id) REFERENCES media_item(media_id)
+);
+
 COMMIT;
