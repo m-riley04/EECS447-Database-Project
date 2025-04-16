fea819aea6034a060494e477bfc8139a2a0621ee
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-16 13:59:14 -0500

    fix environment variable naming

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -6,7 +6,7 @@ interface ImportMetaEnv {
     readonly VITE_DB_PASSWORD: string
     readonly VITE_DB_HOST: string
     readonly VITE_DB_PORT: number
-    readonly VITE_DB_DATABASE: string
+    readonly VITE_DB_NAME: string
 
     readonly VITE_SSH_USERNAME: string
     readonly VITE_SSH_PASSWORD: string
