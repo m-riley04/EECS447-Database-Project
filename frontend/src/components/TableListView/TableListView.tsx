7129a8f3807788ef952859a87e59cf1a7cbfe7be
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-16 14:12:46 -0500

    fix table handling null

--- frontend/src/components/TableListView/TableListView.tsx
+++ frontend/src/components/TableListView/TableListView.tsx
@@ -12,11 +12,11 @@ const TableListView: React.FC<TableListViewProps> = ({
         <table>
             <thead>
                 <tr>
-                    {items.length > 0 ? Object.keys(items[0]).map((key, index) => <th key={index}>{key}</th>) : null}
+                    {items?.length > 0 ? Object.keys(items[0]).map((key, index) => <th key={index}>{key}</th>) : null}
                 </tr>
             </thead>
             <tbody>
-                {items.map((item, index) => <TableListViewItem key={index} item={item} />)}
+                {items?.length > 0 ? items?.map((item, index) => <TableListViewItem key={index} item={item} />) : null}
             </tbody>
         </table>
     )
