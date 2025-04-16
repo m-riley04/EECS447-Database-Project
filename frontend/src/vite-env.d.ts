8182a0c3ca37098e8923b4bcc9f3aa3b5d3e0d7a
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-16 16:02:17 -0500

    remove unnecessary vite env

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,18 +1,1 @@
 /// <reference types="vite/client" />
-
-
-interface ImportMetaEnv {
-    readonly VITE_DB_USERNAME: string
-    readonly VITE_DB_PASSWORD: string
-    readonly VITE_DB_HOST: string
-    readonly VITE_DB_PORT: number
-    readonly VITE_DB_NAME: string
-
-    readonly VITE_SSH_USERNAME: string
-    readonly VITE_SSH_PASSWORD: string
-    readonly VITE_SSH_HOST: string
-    readonly VITE_SSH_PORT: number
-
-    readonly VITE_SERVER_HOST: number
-    readonly VITE_SERVER_PORT: number
-  }
