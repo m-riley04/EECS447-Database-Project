b268ecb789e4b09c3e78bba08911a691382663e7
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-16 17:21:12 -0500

    remove error handling from fetchData

--- frontend/src/server/server_functions.ts
+++ frontend/src/server/server_functions.ts
@@ -3,14 +3,8 @@ export const SERVER_PORT = import.meta.env.VITE_SERVER_PORT || 3001;
 export const SERVER_API_URL = `http://${SERVER_HOST}:${SERVER_PORT}/api`;
 
 export async function fetchData(endpoint: string = '') {
-    try {
-      const response = await fetch(`${SERVER_API_URL}/${endpoint}`);
-      const data = await response.json();
-      return data;
-      // Process the data
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return null;
-    }
-  }
+    const response = await fetch(`${SERVER_API_URL}/${endpoint}`);
+    const data = await response.json();
+    return data;
+}
   
