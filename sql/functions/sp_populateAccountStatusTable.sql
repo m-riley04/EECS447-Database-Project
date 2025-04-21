8de4125eacc45429f887c5ccc78e365c8a728d14
Ryland-Edwards <rylandcedwards@gmail.com>
2025-04-20 22:31:35 -0500

    changed account statuses

--- sql/functions/sp_populateAccountStatusTable.sql
+++ sql/functions/sp_populateAccountStatusTable.sql
@@ -2,11 +2,14 @@ DELIMITER //
 
 CREATE OR REPLACE PROCEDURE spPopulateAccountStatusTable()
 BEGIN
-    INSERT INTO account_status (status_name)
+    INSERT INTO account_status (status_name, status_description)
     VALUES
-        ('New'),
-        ('Regular'),
-        ('Banned');
+        ('New', 'A recently new member'),
+        ('Active', 'An active member'),
+        ('Inactive', 'A member who has not been active for a while'),
+        ('Banned', 'Member has been banned');
 END //
 
+
+CALL spPopulateAccountStatusTable();
 DELIMITER ;
--- sql/tables/t_account_status.sql
+++ sql/tables/t_account_status.sql
@@ -1,4 +1,5 @@
 CREATE TABLE account_status (
     account_status_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    status_name VARCHAR(50) NOT NULL UNIQUE
+    status_name VARCHAR(50) NOT NULL UNIQUE,
+    status_description VARCHAR(255) NOT NULL
 );
