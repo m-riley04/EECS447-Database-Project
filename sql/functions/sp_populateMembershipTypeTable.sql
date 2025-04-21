11211b00db0023ed304002afc9e64f61ea40c587
Ryland-Edwards <rylandcedwards@gmail.com>
2025-04-20 22:33:48 -0500

    fixed populate users function

--- sql/functions/sp_populateMembershipTypeTable.sql
+++ sql/functions/sp_populateMembershipTypeTable.sql
@@ -2,11 +2,12 @@ DELIMITER //
 
 CREATE OR REPLACE PROCEDURE spPopulateMembershipTypeTable()
 BEGIN
-    INSERT INTO membership_type (membership_type_name)
+    INSERT INTO membership_type (membership_type_name, membership_description, borrowing_limit)
     VALUES
-        ('Normal'),
-        ('Student'),
-        ('Researcher');
+        ('Normal', 'The most basic memberdhip type', 10),
+        ('Student', 'Student membership allows discounts and higher borrowing rate', 20),
+        ('Researcher', 'Researcher membership allows higher borrowing rate', 50);
 END //
 
+CALL spPopulateMembershipTypeTable();
 DELIMITER ;
