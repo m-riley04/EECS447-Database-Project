6e34fd661f2f37e58ae9dbb7c56c0a1d8199806f
Ryland Edwards <rylandcedwards@gmail.com>
2025-04-21 23:45:26 -0500

    added more media

--- sql/functions/sp_populateMediaItemTable.sql
+++ sql/functions/sp_populateMediaItemTable.sql
@@ -2,7 +2,7 @@ DELIMITER //
 
 CREATE OR REPLACE PROCEDURE spPopulateMediaItemTable()
 BEGIN
-    INSERT INTO media_item (
+    INSERT IGNORE INTO media_item (
         author_id,
         genre_id,
         media_type_id,
@@ -12,17 +12,24 @@ BEGIN
         isbn
     )
     VALUES
-    (1, 1, 1, "Nineteen Eighty-Four", 1949, 1, "9780151660346"),
-    (1, 1, 1, "Animal Farm", 1945, 1, "9780151002177")
+    (1, 1, 1, 'Nineteen Eighty-Four', 1949, 1, '9780151660346'),
+    (1, 1, 1, 'Animal Farm', 1945, 1, '9780151002177'),
+    (2, 4, 1, 'Fairy Tale', 2022, 1, '9781668002179'),
+    (2, 4, 1, 'Revival', 2014, 1, '9781476770383'),
+    (3, 6, 1,'David Copperfield', 1850, 1, '9780393958287'),
+    (3, 6, 1, 'Bleak House', 1853, 1, '9780679405689'),
+    (4, 8, 1, 'The Raven', 1845, 1, '9781648337062'),
+    (4, 4, 1, 'The Tell-Tale Heart', 1843, 1, '9781648337079'),
+    (5, 1, 1, 'I, Robot', 1950, 1, '9780553294385'),
+    (5, 1, 1, 'The Last Question', 1956, 1, '9781884214493'),
+    (6, 2, 1, 'The Temptest', 1610, 1, '9780143128632'),
+    (6, 2, 1, 'Hamlet', 1603, 1, '9780143128625')
+    (7, );
 
-    ON DUPLICATE KEY UPDATE
-        author_id = VALUES(author_id),
-        genre_id = VALUES(genre_id),
-        media_type_id = VALUES(media_type_id),
-        title = VALUES(title),
-        publication_year = VALUES(publication_year),
-        avialablity = VALUES(avialablity);
 
-END; //
+END //
+ALTER TABLE media_item ADD CONSTRAINT unique_isbn UNIQUE (isbn);
+
+CALL spPopulateMediaItemTable();
 
 DELIMITER;
--- sql/tables/t_media_item.sql
+++ sql/tables/t_media_item.sql
@@ -4,11 +4,12 @@ CREATE TABLE media_item (
     genre_id INT,
     media_type_id INT,
     title VARCHAR(255) NOT NULL CHECK (CHAR_LENGTH(title) >= 1),
-    publication_year INT NOT NULL CHECK (publication_year BETWEEN 1900 AND 2025),
+    publication_year INT NOT NULL CHECK (publication_year BETWEEN 0 AND 2025),
     availability TINYINT NOT NULL CHECK (availability IN (0, 1)),
     isbn VARCHAR(13) NOT NULL CHECK (CHAR_LENGTH(isbn) = 13 AND isbn REGEXP '^(978|979)[0-9]+$'),
     
     CONSTRAINT `fk_author_id` FOREIGN KEY (author_id) REFERENCES author(author_id),
     CONSTRAINT `fk_genre_id` FOREIGN KEY (genre_id) REFERENCES genre(genre_id),
     CONSTRAINT `fk_media_type_id` FOREIGN KEY (media_type_id) REFERENCES media_type(media_type_id)
+    CONTRIANT unique_isbn UNIQUE (isbn)
 );
