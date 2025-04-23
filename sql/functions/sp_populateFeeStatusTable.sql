8563d0c6489c2f2d4d5292540a9ebfda8123c7f8
Alex Doehring <alex.doehring2@gmail.com>
2025-04-22 20:32:18 -0500

    Update sp_populateFeeStatusTable.sql

--- sql/functions/sp_populateFeeStatusTable.sql
+++ sql/functions/sp_populateFeeStatusTable.sql
@@ -7,6 +7,8 @@ BEGIN
         ('Issued', 'The fee is issued to the user'),
         ('Overdue', 'The fee is overdue its payment'),
         ('Paid', 'The fee has been paid');
-END //
+END;
+
+CALL spPopulateFeeStatusTable();
 
 DELIMITER ;
