1f125a9ed4d25f5fc2546abc9fba08d6570789b6
Alex Doehring <alex.doehring2@gmail.com>
2025-04-22 20:25:53 -0500

    Update trans_createTables.sql

--- sql/transactions/trans_createTables.sql
+++ sql/transactions/trans_createTables.sql
@@ -37,7 +37,8 @@ CREATE TABLE membership_type(
 -- From the t_account_status.sql file
 CREATE TABLE account_status (
     account_status_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    status_name VARCHAR(50) NOT NULL UNIQUE
+    status_name VARCHAR(50) NOT NULL UNIQUE,
+    status_description VARCHAR(255) NOT NULL
 );
 
 -- From the t_media_item.sql file
