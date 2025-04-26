6ba5c1c22aa35a699b5330a8130316557dd7b3bf
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-26 14:15:55 -0500

    add user loading and refreshing

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,30 +1,114 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import MediaItemView from '../components/MediaItemView/MediaItemView';
 import { useEffect, useState } from 'react';
-import { fetchData } from '../server/server_functions';
+import { fetchData, getUserById } from '../server/server_functions';
+import CheckoutForm from '../components/CheckoutForm/CheckoutForm';
+import UserModel from '../models/UserModel';
 
 const UserPage = () => {
     const navigate = useNavigate();
     const { userId } = useParams<{ userId: string }>();
 
-    const [mediaItems, setMediaItems] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [user, setUser] = useState<UserModel | null>(null);
 
-    useEffect(() => {
+    const [mediaItemsLoading, setMediaItemsLoading] = useState(true);
+    const [mediaItems, setMediaItems] = useState([]);
+
+    function handleRefreshMediaItems() {
+        console.log('Refreshing media items...');
+
+        // Set states
+        setMediaItemsLoading(true);
+
+        // Fetch items
         fetchData("media_item")
             .then((data) => {
-                setError(null);
-                setLoading(false);
+                setMediaItemsLoading(false);
                 setMediaItems(data)
             })
             .catch((error) => {
-                setError(error);
-                setLoading(false);
+                setMediaItemsLoading(false);
                 setMediaItems([]);
                 console.error('Error fetching data:', error);
             });
-    }, [])
+    }
+
+    function handleRefreshUser() {
+        console.log('Refreshing user...');
+
+        // Get the user based from user id
+        if (!userId || userId === '0') return;
+
+        // Set states
+        setLoading(true);
+        setError(null);
+    
+        // Get the user based from user id
+        getUserById(parseInt(userId))
+            .then((response) => {
+                if (response.length > 0) {
+                    setUser(response[0]);
+                } else {
+                    console.error(`User with ID ${userId} not found.`);
+                    setUser(null);
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching user:', error);
+                setUser(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }
+
+    useEffect(() => {
+        // Fetch media items
+        handleRefreshMediaItems();
+
+        // Handle user refresh
+        handleRefreshUser();
+        
+    }, []);
+
+    if (!userId || userId === '0') {
+        console.error('User ID is not defined. Redirecting to login page.');
+        navigate('/login');
+        return null;
+    }
+
+    if (loading) {
+        return (
+            <>
+                <h1>Loading...</h1>
+                <p>Please wait while we load the page and user data.</p>
+            </>
+        )
+    }
+
+    if (error) {
+        return (
+            <>
+                <h1>Error</h1>
+                <p>Error: {error}</p>
+                <button onClick={handleRefreshUser}>Refresh User</button>
+                <button onClick={() => navigate('/')}>Logout</button>
+            </>
+        )
+    }
+
+    if (!user) {
+        return (
+            <>
+                <h1>Error</h1>
+                <p>Could not find user with id {userId}.</p>
+                <button onClick={handleRefreshUser}>Refresh User</button>
+                <button onClick={() => navigate('/')}>Logout</button>
+            </>
+        )
+    }
 
     return (
         <>
@@ -32,6 +116,9 @@ const UserPage = () => {
             { error ? <p>Error: {error}</p> : null }
             <p>Status: {loading ? "Loading..." : "Idle"}</p>
             <MediaItemView mediaItems={mediaItems ?? []} />
+            <CheckoutForm userId={parseInt(userId)} />
+            <button onClick={handleRefreshUser}>Refresh User</button>
+            <button onClick={handleRefreshMediaItems}>Refresh Media Items</button>
             <button onClick={() => navigate(`/home/${userId}`)}>Home</button>
         </>
     )
