487c7d0ca7ba1b819060838a40c132706460c3cb
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-04-26 11:59:37 -0500

    add constraint for maximum checkouts for users

--- sql/functions/sp_checkOutMediaItem.sql
+++ sql/functions/sp_checkOutMediaItem.sql
@@ -5,8 +5,11 @@ CREATE OR REPLACE PROCEDURE spCheckoutMediaItem(
     p_user_id INT
 )
 BEGIN
-    -- Check if the media item is available
+    -- Declare variables
+    DECLARE user_checkout_count INT;
     DECLARE media_available TINYINT;
+
+    -- Check if the media item is available
     SELECT availability INTO media_available FROM media_item WHERE media_id = p_media_id;
 
     IF media_available IS NULL THEN
@@ -17,8 +20,19 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Media item is not available for checkout.';
     END IF;
 
+    -- Check if the user doesn't have 5 items check out already
+    SELECT COUNT(*) INTO user_checkout_count
+    FROM `transaction`
+    WHERE user_id = p_user_id AND return_date IS NULL; -- Only count items that are currently checked out
+
+    IF user_checkout_count >= 5 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User has already checked out the maximum number of items.';
+    END IF;
+
+    -- Update media item availability
     UPDATE media_item SET `availability` = False WHERE media_id = p_media_id;
 
+    -- Create a new transaction record
     INSERT INTO `transaction` (user_id, media_id, checkout_date, due_date, return_date)
     VALUES (p_user_id, p_media_id, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 14 DAY), NULL); -- 14 days (2 weeks) for due date
 END; //
