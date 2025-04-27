ef0a6914a326a0d7d4a9a90e4f0c53d0bcb484c8
Alex Doehring <alex.doehring2@gmail.com>
2025-04-27 16:04:21 -0500

    Create sp_populateFeeTable.sql

---
+++ sql/functions/population/sp_populateFeeTable.sql
@@ -0,0 +1,23 @@
+DELIMITER //
+
+CREATE OR  REPLACE PROCEDURE spPopulateFeeTable()
+BEGIN
+    INSERT IGNORE INTO fee (
+        user_id,
+        date_issued,
+        amount,
+        fee_status_id
+        )
+        VALUES
+        # Values here
+        (1, '2025-02-01', 5.00, 1),
+        (2, '2025-02-15', 10.00, 2),
+        (3, '2025-03-05', 10.00, 1),
+        (4, '2025-03-20', 10.00, 2),
+        (5, '2025-03-30', 5.00, 1);
+
+END;
+
+CALL spPopulateFeeTable();
+
+DELIMITER;
